158b16150df73fef0bafba98054163a544463f41
Babajide Owosakin <babajide.owosakin@getyourguide.com>
2020-01-13 12:36:53 +0100

    Add readme

---
+++ string-concat-format.py
@@ -0,0 +1,18 @@
+import os
+
+names = ["Jide", "Taiwo"]
+
+for name in names:
+    statement = 'Hello dear ' + name
+    print(statement)
+
+location_of_files = '/Users/babajide.owosakin/myspace/python-refresh'
+file_name = 'filename.txt'
+
+with open(os.path.join(location_of_files, file_name)) as f:
+    print(f.read())
+
+who: str = 'Jide'
+how_many: int = 12
+
+print('{} bought {} apples today'.format(who, how_many))
