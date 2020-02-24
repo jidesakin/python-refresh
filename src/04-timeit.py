3ed77d421d426546a0250b0916c776a2dcb19e4c
Babajide Owosakin <babajide.owosakin@getyourguide.com>
2020-02-24 16:29:54 +0100

    using timeit

---
+++ src/04-timeit.py
@@ -0,0 +1,17 @@
+import timeit
+
+input_list = range(100)
+
+def div_by_five(num):
+    if num % 5 == 0:
+        return True
+    else:
+        return False
+
+# generator converted to list
+xyz = list(i for i in input_list if div_by_five(i))
+
+# generator:
+xyz = [i for i in input_list if div_by_five(i)]
+
+
